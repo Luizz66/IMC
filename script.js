8088279caf2747b0a275fd20698f16a4a5561376
Luiz <luizgustavoka66@gmail.com>
2025-07-18 18:44:54 -0300

    calculate event

--- script.js
+++ script.js
@@ -10,3 +10,95 @@ w.addEventListener("click", () => {
   w.classList.add("w-active");
   m.classList.remove("m-active");
 });
+
+// Impedir entrada não numérica nos campos
+function isNumberKey(e) {
+  return (
+    (e.key >= "0" && e.key <= "9") ||
+    ["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Tab"].includes(e.key)
+  );
+}
+
+const calc = document.querySelector("#box-btn");
+calc.addEventListener("click", () => {
+  if (calc.classList.contains("btn-pressed")) {
+    location.reload();
+    window.scrollTo(0, 0);
+    return;
+  }
+
+  const idade = document.querySelector("#idade").value;
+  const altura = document.querySelector("#altura").value;
+  const peso = document.querySelector("#peso").value;
+
+  if (!idade || !altura || !peso) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  const imc = (peso / ((altura / 100) * (altura / 100))).toFixed(2);
+  let classification;
+
+  const txtResult = document.querySelector("#txt-result");
+  const boxResult = document.querySelector("#box-result");
+
+  boxResult.style.display = "flex";
+  boxResult.scrollIntoView({ behavior: "smooth", block: "center" });
+
+  const lines = document.querySelectorAll(".line");
+  war = true;
+  if (imc < 18.5) {
+    lines[0].style.color = "red";
+    classification = "Abaixo do peso";
+  } else if (imc < 24.9) {
+    lines[1].style.color = "green";
+    classification = "Peso normal";
+    war = false;
+  } else if (imc < 29.9) {
+    lines[2].style.color = "red";
+    classification = "Sobrepeso";
+  } else if (imc < 34.9) {
+    lines[3].style.color = "red";
+    classification = "Obesidade grau I";
+  } else if (imc < 39.9) {
+    lines[4].style.color = "red";
+    classification = "Obesidade grau II";
+  } else {
+    lines[5].style.color = "red";
+    classification = "Obesidade grau III";
+  }
+
+  txtResult.textContent = `Seu IMC é ${imc} e você está classificado com: ${classification}`;
+
+  formatBoxResult();
+  iconStyle(war);
+});
+
+function formatBoxResult() {
+  document.querySelector("#idade").value = "";
+  document.querySelector("#altura").value = "";
+  document.querySelector("#peso").value = "";
+  m.classList.remove("m-active");
+  w.classList.remove("w-active");
+
+  const icone = document.createElement("ion-icon");
+  icone.setAttribute("name", "refresh-outline");
+  calc.innerHTML = "";
+  calc.append(icone);
+  calc.classList.add("btn-pressed");
+}
+
+function iconStyle(bool) {
+  const icon = document.querySelector("#icon");
+  if (bool) {
+    icon.name = "warning-outline";
+    icon.classList.add("icon-red");
+  } else {
+    icon.classList.add("icon-green");
+    icon.style.color = green;
+  }
+}
+
+window.addEventListener("load", function () {
+  window.scrollTo(0, 0);
+});
